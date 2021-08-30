7004e3dd7cf2766486c6e7ff14fa13eeccf99646
gabriele rugani <gabriele.rugani@apparound.com>
2021-08-30 18:48:00 +0200

    aggiunto pseudocodice in mainFragment2ViewModel

--- app/src/main/java/com/aeroxr1/multiplepopupusecase/ui/mainfragment2/MainFragment2ViewModel.kt
+++ app/src/main/java/com/aeroxr1/multiplepopupusecase/ui/mainfragment2/MainFragment2ViewModel.kt
@@ -3,5 +3,29 @@ package com.aeroxr1.multiplepopupusecase.ui.mainfragment2
 import androidx.lifecycle.ViewModel
 
 class MainFragment2ViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    fun clickOnUpdateButton(){
+        // da qui in poi sia per schermata 1 che per schermata 2
+        if(shouldShowPopupUrlUseCase(json)){
+            showPopupUrl("title", "message", "urlToOpen")
+        } else if(showUrlUseCase(json)){
+            openUrl("title", "message")
+        } else if (shouldUpdateApp()){
+            showDownloadProgressDialog()
+            downloadAppUseCase()
+            //gestione progress
+        }
+
+    }
+
+    fun clickOkOnPopupUrl(){
+        if(showUrlUseCase(json)){
+            openUrl("title", "message")
+        } else if (shouldUpdateApp()){
+            showDownloadProgressDialog()
+            downloadAppUseCase()
+            //gestione progress
+        }
+    }
+
 }
